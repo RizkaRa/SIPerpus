5878367dea81ed1be29eddce9a401bd431627ab8
Andrian <andrianramadhan99@gmail.com>
2021-04-24 22:19:50 +0700

    first commit

---
+++ about/index.php
@@ -0,0 +1,16 @@
+<?php include('../template/header.php');?>
+<section id="main-content">
+    <section class="wrapper site-min-height">
+        <div class="row content-panel">
+            <h2 class="centered"><i class="fa fa-info-circle"></i> Kelompok 4</h2><hr>
+            <div class="col-md-8">
+                <h4>1. Andrian Rizka Ramadhan (201851114)</h4>
+                <h4>2. Muhammad Rifqi Mubarok (201851137)</h4>
+                <h4>3. Rifqi Dwiyana Firmandito (201851140)</h4>
+                <h4>4. Rendy Octaviano Valentino Kiky (201851142)</h4>
+                <h4>5. Nurul Mustofa (201851143)</h4>
+            </div>
+        </div>
+    </section>
+</section>
+<?php include('../template/footer.php');?>
